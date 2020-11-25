b265caf778491546431ceae926bf0a10aca0ce84
WEBSTER UNIVERSITY <DTS@WEBSTER.EDU>
2020-11-25 15:52:08 -0600

    Added Futuristic Door and Smooth Up/Down Movement

--- Assets/_Austan/Code/DoorTrigger.cs
+++ Assets/_Austan/Code/DoorTrigger.cs
@@ -1,21 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DoorTrigger : MonoBehaviour
 {
     [SerializeField] 
     GameObject door;
 
-    bool isOpened = false; 
+    private float _doorMoveDistance;
+    private float _initialYPosition;
+    private float _doorMoveDuration;
+
+    bool isOpened = false;
+
+    private void Awake()
+    {
+        _initialYPosition = door.transform.position.y;
+        _doorMoveDistance = 5;
+        _doorMoveDuration = 1;
+    }
 
     void OnTriggerEnter(Collider col)
     {
         if (isOpened == false)
         {
             isOpened = true;
-            door.transform.position += new Vector3(0, 4, 0);
-
+            door.transform.DOMoveY(_doorMoveDistance, _doorMoveDuration);
         }
     }
 
@@ -24,7 +35,9 @@ public class DoorTrigger : MonoBehaviour
         if (isOpened == true)
         {
             isOpened = false;
-            door.transform.position -= new Vector3(0, 4, 0);
+            //door.transform.position -= new Vector3(0, 4, 0);
+
+            door.transform.DOMoveY(_initialYPosition, _doorMoveDuration);
         }
     }
 }
