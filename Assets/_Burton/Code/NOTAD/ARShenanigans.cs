357ce18e66a28708447db56c078502116bfc6c91
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2021-03-28 18:08:30 -0500

    ar stuff

---
+++ Assets/_Burton/Code/NOTAD/ARShenanigans.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ARShenanigans : MonoBehaviour
+{
+    public Button velocityButton; 
+
+    void Start()
+    {
+        velocityButton.onClick.AddListener(ChangeDwarfVelocity);
+    }
+
+    void ChangeDwarfVelocity()
+    {
+        Rigidbody rigidbody = FindObjectOfType<Dwarf>().GetComponent<Rigidbody>();
+        rigidbody.velocity = -rigidbody.velocity;
+    }
+}
